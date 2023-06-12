cea08adab8ddc8daab450e6de8bfd682032d717d
JC <jasonqc9103@gmail.com>
2023-06-12 09:31:43 -0400

    add css & 404 template

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-
+from django.template.loader import render_to_string
 
 
 # Create your views here.
@@ -67,4 +67,6 @@ def monthly_challenge(request, month):
         })
        
     except:
-        return HttpResponseNotFound("<h1>This month is not supported !</h1>")
+        # response_data = render_to_string("404.html")
+        # return HttpResponseNotFound(response_data)
+        raise Http404()
