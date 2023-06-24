e116b68b8063faa5ac507570dc0d2e7588c53558
JC <jasonqc9103@gmail.com>
2023-06-24 12:51:35 -0400

    create admin pages

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,11 +1,25 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
 
 class Book(models.Model):
   title = models.CharField(max_length=50)
-  rating = models.IntegerField()
+  rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+  author = models.CharField(max_length=50, null=True, blank=True)
+  is_bestselling = models.BooleanField(default=False)
+  slug = models.SlugField(default="", editable=False, blank=True, null=False, db_index=True) # Harry Potter 1 => harry-potter-1
+  
+  
+  def get_absolute_url(self):
+      return reverse("book-detail", args=[self.id])
+  
+  # def save(self, *args, **kwargs):
+  #   self.slug = slugify(self.title)
+  #   super().save(*args, **kwargs)
   
   def __str__(self):
     return f"{self.title} ({self.rating})"
