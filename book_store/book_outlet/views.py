077cf3bb2daa5a11894cb00b1e35a9d9709bf629
JC <jasonqc9103@gmail.com>
2023-06-24 20:07:39 -0400

    add model relations 1-many,many-many,1-1

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Book
 from django.http import Http404
+from django.db.models import Avg
 # Create your views here.
 
 
 
 def index(request):
   books = Book.objects.all()
+  num_books = books.count()
+  avg_rating = books.aggregate(Avg("rating")) # rating__avg, rating__min
   return render(request, "book_outlet/index.html", {
-    "books": books
+    "books": books,
+    "total_books": num_books,
+    "average_rating": avg_rating
   })
   
   
-def book_detail(request, id):
+def book_detail(request, slug):
   # try:
   #   book = Book.objects.get(pk=id)
   # except:
   #   raise Http404()
-  book = get_object_or_404(Book, pk=id)
+  book = get_object_or_404(Book, slug=slug)
   return render(request, "book_outlet/book_detail.html", {
     "title": book.title,
     "author": book.author,
