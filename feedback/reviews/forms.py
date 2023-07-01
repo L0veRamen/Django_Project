64529201ab3abc0717b5323c60c186cdb8d8c2c5
JC <jasonqc9103@gmail.com>
2023-07-01 12:03:42 -0400

    add Form module

---
+++ feedback/reviews/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from .models import Review
+
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(
+#         label="Your Name",
+#         max_length=10,
+#         error_messages={
+#             "required": "Your name must not be empty",
+#             "max_length": "Please enter a shorter name!"
+#         })
+#     review_text = forms.CharField(
+#         label="Your Feedback",
+#         max_length=200,
+#         widget=forms.Textarea
+#     )
+#     rating = forms.IntegerField(
+#         label="Your Rating",
+#         min_value=1,
+#         max_value=5
+#     )
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = "__all__"
+        labels = {
+            "user_name": "Your Name",
+            "review_text": "Your Feedback",
+            "rating": "Your Rating"
+        }
+        error_messages = {
+            "user_name": {
+                "required": "Your name must not be empty",
+                "max_length": "Please enter a shorter name!"
+            }
+        }
+        widgets = {
+            "review_text": forms.Textarea(attrs={
+                "placeholder": "Enter your feedback here",
+            })
+        }
---
+++ feedback/reviews/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path("", views.review, name='review'),
+    path("", views.ReviewView.as_view(), name='review'),
+    path("thank-you", views.thank_you, name="thank_you")
+    
+]
