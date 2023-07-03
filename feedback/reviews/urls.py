c7ec7c3b52419c8b545ec95df0af0775ab3e9909
JC <jasonqc9103@gmail.com>
2023-07-03 19:03:52 -0400

    add review session modules

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     # path("thank-you", views.thank_you, name="thank_you"),
     path("thank-you", views.ThankYouView.as_view(), name='thank_you'),
     path("reviews", views.ReviewListView.as_view(), name='reviews_list'),
+    path("reviews/favorite", views.AddFavoriteView.as_view(), name='add_favorite'),
     # path("reviews/<int:id>", views.SingleReviewView.as_view(), name='single_review'),
     path("reviews/<int:pk>", views.SingleReviewView.as_view(), name='single_review'),
 ]
